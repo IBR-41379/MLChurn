e9e0f96c5f37773808e176e4c73946a6b8d6d21d
IBR-41379 <sayandeep369@gmail.com>
2023-07-30 20:44:11 +0530

    Adding the files

---
+++ ChurnCal.py
@@ -0,0 +1,33 @@
+#PRedicting Customer Churn rate using Decision Tree
+#Dataset from Kaggle: https://www.kaggle.com/code/korfanakis/predicting-customer-churn-with-machine-learning/input
+import sys
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+import sklearn.tree as tree
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("Churn_Modelling.csv")
+#prints the first 5 rows of the dataset
+#print(df.head())
+
+#Sorting the dependent and independent values
+x=df[['CreditScore','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember','EstimatedSalary']].values
+y=df['Exited']
+
+#Splitting the dataset into training and testing data
+x_test,x_train,y_test,y_train=train_test_split(x,y,test_size=0.3,random_state=3)
+
+#Creating the decision tree classifier
+decTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
+decTree.fit(x_train,y_train)
+
+#Predicting the values
+predTree = decTree.predict(x_test)
+print("Accuracy of the model: ", metrics.accuracy_score(y_test, predTree))
+
+#Visualizing the decision tree
+# tree.plot_tree(decTree)
+# plt.show()
